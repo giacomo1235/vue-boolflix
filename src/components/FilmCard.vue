f386d213d2188ffcf6cb5fccb7fcd6a9a4d0d11b
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-29 13:07:44 +0200

    funziona ma senza ricerca dinamica

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <img :src="`https://image.tmdb.org/t/p/w500/${filmData.poster_path}`" :alt="filmData.original_title">
+      <h1>{{filmData.title}}</h1>
+      <h2>{{filmData.original_title}}</h2>
+      <h3>{{filmData.original_language}}</h3>
+      <h4>{{filmData.vote_average}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilmCard',
+  props: {
+    filmData: Object
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,12 +1,28 @@
 <template>
-  <p>{{searchString}}</p>
+  <div>
+      <p>{{searchString}}</p>
+      <FilmCard v-for="film in arrFilms" :key="film.title" :film-data="film" />
+  </div>
 </template>
 
 <script>
+import FilmCard from './FilmCard.vue'
+import axios from 'axios'
 export default {
   name: 'MainBoolflix',
+  data () {
+    return {
+      arrFilms: null
+    }
+  },
   props: {
     searchString: String
+  },
+  components: {
+    FilmCard
+  },
+  created () {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&query=la').then((response) => { this.arrFilms = response.data.results })
   }
 }
 </script>
